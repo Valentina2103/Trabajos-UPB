6278a479c064fa2741ace0fd2d6adc356d5c4349
Valentina2103 <80237175+Valentina2103@users.noreply.github.com>
2021-03-24 14:18:57 -0500

    Update Lab22_2403_Bucles_Anidados.py

--- Lab22_2403_Bucles_Anidados.py
+++ Lab22_2403_Bucles_Anidados.py
@@ -1,9 +1,9 @@
 #Ejercicio1
 for i in range(6):
     for j in range(4):
-        print("i vale:", i, "y j vale:",j)
+        print("i vale:", i, "y  j vale:",j)
 
 #Ejercicio2
 for i in range (5):
     for j in range(i):
-        print("i vale:", i, "y j vale:", j)
+        print("i vale:", i, "y  j vale:", j)
